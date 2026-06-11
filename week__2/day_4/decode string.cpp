2629d4bbf866cd9b60bac6a4c3c41394e32995b4
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-11 22:04:18 +0530

    Update decode string.cpp

--- week__2/day_4/decode string.cpp
+++ week__2/day_4/decode string.cpp
@@ -1,1 +1,24 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        count_stack = []
+        string_stack = []
 
+        current_string = ""
+        current_number = 0
+
+        for ch in s:
+            if ch.isdigit():
+                current_number = current_number * 10 + int(ch)
+            elif ch == '[':
+                count_stack.append(current_number)
+                string_stack.append(current_string)
+                current_number = 0
+                current_string = ""
+            elif ch == ']':
+                repeat = count_stack.pop()
+                prev = string_stack.pop()
+                current_string = prev + current_string * repeat
+            else:
+                current_string += ch
+
+        return current_string
