b1f77c8399c2acb42be854409a716cb8ba7d05fd
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-20 20:34:34 +0530

    Update daily temp.cpp

--- week__3/day_4/daily temp.cpp
+++ week__3/day_4/daily temp.cpp
@@ -1,1 +1,22 @@
+#include <vector>
+#include <stack>
+using namespace std;
 
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ans(temperatures.size(), 0);
+        stack<int> st;
+
+        for (int i = 0; i < temperatures.size(); i++) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prev = st.top();
+                st.pop();
+                ans[prev] = i - prev;
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
