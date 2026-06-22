8b02f8a75f3ea286f46a01d6ec8f06dd6fdd3ffe
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-22 19:22:19 +0530

    Update max depth of binary tree.cpp

--- week__4/day_1/max depth of binary tree.cpp
+++ week__4/day_1/max depth of binary tree.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    void solve(TreeNode* root, int &ans, int len) {
+        if(!root) {
+            ans = max(ans, len);
+            return;
+        }
+        solve(root->left, ans, len+1);
+        solve(root->right, ans, len+1);
+    }
+    int maxDepth(TreeNode* root) {
+        int ans = 0;
+        solve(root, ans, 0);
+        return ans;
+    }
+};
