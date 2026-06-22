970388dcb47ca4bd514413b6d33d59016145c350
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-22 19:23:42 +0530

    Update same tree.cpp

--- week__4/day_1/same tree.cpp
+++ week__4/day_1/same tree.cpp
@@ -1,1 +1,12 @@
-
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+   if(p==NULL||q==NULL){
+    return p==q;
+   }
+   bool isrightsame =isSameTree(p->right,q->right);
+   bool isleftsame =isSameTree(p->left,q->left);
+   return isleftsame && isrightsame && p->val == q->val;
+    }
+};
