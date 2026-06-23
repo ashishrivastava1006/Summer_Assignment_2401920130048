401472afd0165a261d648e2c94ba52a5b61b3bef
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-23 23:39:50 +0530

    Update binary tree inorder .cpp

--- week__4/day_2/binary tree inorder .cpp
+++ week__4/day_2/binary tree inorder .cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+vector <int>ans;
 
+      void   inorder(TreeNode* node){
+            if(node==NULL)
+            return;
+            inorder(node->left);
+            ans.push_back(node->val);
+            inorder(node->right);
+      }
+    vector<int> inorderTraversal(TreeNode* root) {
+      inorder(root);
+       return ans;
+        }
+    };
