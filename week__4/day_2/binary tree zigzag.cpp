094588a05b23ad1ae220bcb6d384822ba96c4236
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-23 23:41:23 +0530

    Update binary tree zigzag.cpp

--- week__4/day_2/binary tree zigzag.cpp
+++ week__4/day_2/binary tree zigzag.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>result;
+        if(root==NULL){
+            return result;
 
+        }
+        queue<TreeNode*>nodequeue;
+        nodequeue.push(root);
+        bool leftToright=true;
+        while(!nodequeue.empty()){
+            int size=nodequeue.size();
+            vector<int>row(size);
+            for(int i=0;i<size;i++){
+                TreeNode*node=nodequeue.front();
+                nodequeue.pop();
+                int index=(leftToright)?i:(size-1-i);
+                row[index]=node->val;
+                if(node->left){
+                    nodequeue.push(node->left);}
+                    if(node->right){
+                    nodequeue.push(node->right);
+                }
+            }leftToright=!leftToright;
+            result.push_back(row);
+            }
+            return result;
+        
+    }
+};
