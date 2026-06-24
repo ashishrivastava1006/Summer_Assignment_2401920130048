efc914b7323b4b82771ccb105626a358ff409b29
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-24 23:22:19 +0530

    Update lowest common ancestor.cpp

--- week__4/day_3/lowest common ancestor.cpp
+++ week__4/day_3/lowest common ancestor.cpp
@@ -1,1 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        while(root) {
+            if(p->val < root->val && q->val < root->val) // both are smaller, goto left 
+                root = root->left;
+            else if(p->val > root->val && q->val > root->val) // both are larger, goto right
+                root = root->right;
+            else
+                return root; 
+        }
+
+        return NULL;
+        
+    }
+};
