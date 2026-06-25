8ea00a6b52b98f3a32fc8b38f18213a4ca405cc2
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-25 10:35:20 +0530

    Update binary tree max sum path.cpp

--- week__4/day_4/binary tree max sum path.cpp
+++ week__4/day_4/binary tree max sum path.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+ 
+    int maxPathdown(TreeNode* node,int &maxi){
+        if(node==NULL)return 0;
+          int left=max(0,maxPathdown(node->left,maxi));
+  int right=max(0,maxPathdown(node->right,maxi));
+  maxi=max(maxi,left+right+node->val);
+  return max(left,right)+node->val;
+    }
+int maxPathSum(TreeNode* root) {
+     int maxi = INT_MIN;
+       maxPathdown(root,maxi);
+     return maxi;
+    }
+};
