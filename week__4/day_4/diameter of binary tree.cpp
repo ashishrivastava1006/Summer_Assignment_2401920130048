907acfb317410a3de737cc242eae8ac29e833f5c
Ashi Shrivastava <ashishrivastava1006@gmail.com>
2026-06-25 10:34:17 +0530

    Update diameter of binary tree.cpp

--- week__4/day_4/diameter of binary tree.cpp
+++ week__4/day_4/diameter of binary tree.cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+int height(TreeNode* root){
+    if (root== NULL){
+    return 0;
+}
+int leftheight=height(root->left);
+int rightheight=height(root->right);
+return max(leftheight,rightheight)+1;}
+    int diameterOfBinaryTree(TreeNode* root) {
+if (root== NULL){
+    return 0;
+}    
+int leftdiameter=diameterOfBinaryTree(root->left);
+int rightdiameter=diameterOfBinaryTree(root->right);
+int currdiameter= height(root->left)+height(root->right);
+return max(currdiameter,max(leftdiameter,rightdiameter));
+
+    }
+};
